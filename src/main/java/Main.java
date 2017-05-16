23d57133324d1ab2e57472e3aa1a482571bdb491
blakel86 <blake.laroux86@gmail.com>
2017-05-17 07:59:46 +0800

    updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.setProperty("http.proxyHost", "DCA-WBSAPP-P001.rac.com.au");
         System.setProperty("http.proxyPort", "80");
 
+        //Check if Server is already running
         if (!Server.isProcessRunning("MongoDB")) {
 
             Server server = new Server();
@@ -22,8 +23,10 @@ public class Main {
             }
         }
 
+        //Create new Connection to MongoDB
         Mongo mongo = new Mongo();
 
+        //Check if the Database and Table exists
         if(Mongo.checkDatabaseStatus()){
             Dialog dialog = new Dialog();
             if(Dialog.parameter != "Query the existing database"){
@@ -40,6 +43,7 @@ public class Main {
             SearchGUI search = new SearchGUI();
         }
 
+        //Close Connection and stop server if SearchGUI Closed
         if(!SearchGUI.searchGUIFrame.isVisible()) {
             Mongo.mongoClose();
             Server.stopServer();
